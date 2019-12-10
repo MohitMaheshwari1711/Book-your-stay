274848a9b1aa9b9065669ae463b227c3dc496dea
Mohitmaheshwari1711 <mohitmaheshwari1711@gmail.com>
2019-12-10 17:03:12 +0530

    Minor change

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DB_URI: process.env.DB_URI,
+    SECRET: process.env.SECRET,
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 const cors = require('cors');
 const Rental = require('./models/rental');
+const path = require('path');
 
 const rentalRoutes = require('./routes/rentals');
 const userRoutes = require('./routes/users');
@@ -12,8 +13,10 @@ const bookingRoutes = require('./routes/bookings');
 const imageUploadRoutes = require('./routes/image-upload');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
-    const fakeDb = new FakeDb();
-    // fakeDb.seedDb();
+    if (process.env.NODE_ENV !== 'production') {
+        const fakeDb = new FakeDb();
+        // fakeDb.seedDb();
+    }
 });
 
 const app = express();
@@ -27,8 +30,17 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 app.use('/api/v1', imageUploadRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join(__dirname, '..', 'build');
+    app.use(express.static(appPath));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("I am listening");
 });
